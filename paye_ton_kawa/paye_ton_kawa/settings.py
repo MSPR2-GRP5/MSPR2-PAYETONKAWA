edc53045238b212beda2a92f188288fabe466b95
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-06-10 16:04:54 +0200

    add .env support to container

--- paye_ton_kawa/paye_ton_kawa/settings.py
+++ paye_ton_kawa/paye_ton_kawa/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f45e%o8s-#m=kvimy-@he86^rqve)!!47b@um_)qrs(oc-dm11'
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = bool(os.environ.get("DJANGO_DEBUG", False))
 
-ALLOWED_HOSTS = []
+# 'ALLOWED_HOSTS' should be a single string of hosts with a space between each
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", 'localhost,127.0.0.1').split(",")
 
 
 # Application definition
