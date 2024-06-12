a603a3d4cd52140d6da03a4e424093117e208f7d
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-06-12 11:23:02 +0200

    add mock unit test to test ci

--- paye_ton_kawa/webshop/tests.py
+++ paye_ton_kawa/webshop/tests.py
@@ -1,3 +1,10 @@
 from django.test import TestCase
 
-# Create your tests here.
+from django.test import TestCase
+
+
+class MockTestCase(TestCase):
+
+    def test_mock_test(self):
+        self.assertEqual(True, True)
+        print("Mock testcase successfully run")
