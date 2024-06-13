85dfba9f7445be06c0a73216bd11b2fa3dacbab6
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-06-13 10:42:44 +0200

    fix mypy

--- paye_ton_kawa/webshop/tests.py
+++ paye_ton_kawa/webshop/tests.py
@@ -1,10 +1,8 @@
 from django.test import TestCase
 
-from django.test import TestCase
-
 
 class MockTestCase(TestCase):
 
-    def test_mock_test(self):
+    def test_mock_test(self) -> None:
         self.assertEqual(True, True)
         print("Mock testcase successfully run")
