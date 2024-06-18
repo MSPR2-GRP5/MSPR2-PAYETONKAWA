d2c495316a91c173d329a483cc1558dec9a6550f
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-06-18 16:24:05 +0200

    add models

--- paye_ton_kawa/webshop/models.py
+++ paye_ton_kawa/webshop/models.py
@@ -1,1 +1,55 @@
-# Create your models here.
+from datetime import datetime
+# https://stackoverflow.com/questions/6578986/how-to-convert-json-data-into-a-python-object
+
+
+class Clients:
+    def __init__(self, id: int, last_name: str, first_name: str, address: str):
+        self.id = id
+        self.last_name = last_name
+        self.first_name = first_name
+        self.address = address
+
+    def __str__(self) -> str:
+        return "{0} {1}".format(self.first_name, self.last_name)
+
+
+class Products:
+    def __init__(
+        self,
+        id: int,
+        name: str,
+        description: str,
+        import_location: str,
+        price: int,
+        stocks: int,
+    ):
+        self.id = id
+        self.name = name
+        self.description = description
+        self.import_location = import_location
+        self.price = price
+        self.stocks = stocks
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Command:
+    def __init__(
+        self,
+        id: int,
+        client_id: int,
+        product_id: int,
+        date: datetime,
+        quantity: int,
+        price: int,
+    ):
+        self.id = id
+        self.client_id = client_id
+        self.product_id = product_id
+        self.date = date
+        self.quantity = quantity
+        self.price = price
+
+    def __str__(self) -> str:
+        return "{0}: {1}, {2}".format(self.date, self.client_id, self.product_id)
