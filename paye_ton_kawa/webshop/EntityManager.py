c1105b43d27abd93285c80d4cb588b11367506e2
LorisPercin <74607168+LorisPercin@users.noreply.github.com>
2024-06-20 15:24:08 +0200

    refactor to use EntityManager

---
+++ paye_ton_kawa/webshop/EntityManager.py
@@ -0,0 +1,100 @@
+from pathlib import Path
+from typing import Any, Final
+
+from webshop.models import Order, Product, Customer
+
+import requests
+import json
+import environ
+
+
+env = environ.Env()
+env.read_env(str(Path(__file__).resolve().parent.parent / ".env"))
+
+API_SETTINGS: Final[dict[str, dict[str, str]]] = {
+    "customer": {"api_key": env("API_TOKEN_CUSTOMER"), "url": "localhost:8001"},
+    "product": {"api_key": "", "url": "localhost:8002"},
+    "order": {"api_key": "", "url": "localhost:8003"},
+}
+
+
+def request(api_name: str, parameters: dict[str, Any]) -> Any:
+    settings = API_SETTINGS[api_name]
+    url = f"http://{settings['url']}/api/{api_name}"
+    print("=-=-=-=-=-=-=-=-=-=-=-=-=\n", url)
+    headers = {
+        "accept": "application/json",
+        "X-API-Key": settings["api_key"],
+    }
+
+    try:
+        response = requests.get(url, params=parameters, headers=headers)
+    # The error returned is not the standard ConnectionError, it is a specific requests error with the same name
+    except requests.exceptions.ConnectionError:
+        return {"error": "Connection failed. Is the API server running ?"}
+
+    return json.loads(response.text)
+
+
+def json_to_object(
+    object_json: Any, object_name: str
+) -> list[Customer] | list[Product] | list[Order] | list[None]:
+    match object_name:
+        case "customer":
+            customers: list[Customer] = []
+            for customer_dict in object_json:
+                customer = Customer(
+                    customer_dict["id"],
+                    customer_dict["Nom"],
+                    customer_dict["Prenom"],
+                    customer_dict["Adresse"],
+                )
+                customers.append(customer)
+            return customers
+
+        case "product":
+            products: list[Product] = []
+            for product_dict in object_json:
+                product = Product(
+                    product_dict["id"],
+                    product_dict["product_name"],
+                    product_dict["description"],
+                    product_dict["import_location"],
+                    product_dict["price"],
+                    product_dict["stocks"],
+                )
+                products.append(product)
+            return products
+
+        case "order":
+            orders: list[Order] = []
+            for order_dict in object_json:
+                order = Order(
+                    int(order_dict["idCommande"]),
+                    order_dict["idClient"],
+                    order_dict["idProduit"],
+                    order_dict["dateCommande"],
+                    int(order_dict["quantite"]),
+                    int(order_dict["prix"]),
+                )
+                orders.append(order)
+            return orders
+    return []
+
+
+class EntityManager:
+    @staticmethod
+    def get_all(
+        api_name: str,
+    ) -> list[Customer] | list[Product] | list[Order] | list[None] | dict[str, Any] :
+        # Exploit the API to get all the entities without knowing the fields name
+        params = {"gibberish": "random"}
+
+        response = request(api_name, params)
+
+        if "error" in response:
+            return response
+
+        return json_to_object(response, api_name)
+
+    # def get_entity_by(self, model: Customer|Product|Order, fields: {str: Any}) -> Customer|Product|Order:
--- paye_ton_kawa/webshop/views.py
+++ paye_ton_kawa/webshop/views.py
@@ -1,63 +1,18 @@
-import json
+from typing import Any
 
+from django.http import HttpResponse
 from django.shortcuts import render
 
-from webshop.models import Customer, Product
-import requests
+from webshop.EntityManager import EntityManager
 
 
-def index(request):
-    customers_json = get_request("customer", {"Id": ""})
-    customers: list[Customer] = []
-
-    for customer_dict in customers_json:
-        # Could have used customer = Customer(**customer_dict) if the arguments names were the same
-        customer = Customer(
-            customer_dict["id"],
-            customer_dict["Nom"],
-            customer_dict["Prenom"],
-            customer_dict["Adresse"],
-        )
-        customers.append(customer)
-
-    products_json = get_request("product", {"id": ""})
-    products: list[Product] = []
-
-    for product_dict in products_json:
-        product = Product(
-            product_dict["id"],
-            product_dict["product_name"],
-            product_dict["description"],
-            product_dict["import_location"],
-            product_dict["price"],
-            product_dict["stocks"],
-        )
-        products.append(product)
+def index(request: Any) -> HttpResponse:
+    customers = EntityManager.get_all("customer")
+    products = EntityManager.get_all("product")
+    orders = EntityManager.get_all("order")
 
     return render(
-        request, "webshop/index.html", {"customers": customers, "products": products}
+        request,
+        "webshop/index.html",
+        {"customers": customers, "products": products, "orders": orders},
     )
-
-
-def get_request(api: str, params: dict) -> dict:
-    options = {
-        "customer": {
-            "api_key": "fFZz3dds.vDhQdMclOW4Tw4Ny15QRKxsnQWoA64ifrsv6wNlPeG5YMk2M59HIuUEu",
-            "port": 8001,
-        },
-        "product": {"api_key": "", "port": 8002},
-    }
-
-    url = f"http://localhost:{options[api]['port']}/api/{api}"
-    headers = {
-        "accept": "application/json",
-        "X-API-Key": options[api]["api_key"],
-    }
-
-    try:
-        response = requests.get(url, params=params, headers=headers)
-    # The error returned is not the standard ConnectionError, it is a specific requests error with the same name
-    except requests.exceptions.ConnectionError:
-        return {"error": "Connection failed. Is the API server running ?"}
-
-    return json.loads(response.text)
