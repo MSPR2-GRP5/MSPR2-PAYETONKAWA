bc5f5096fc28cdc154b1629cb3f1ed4cdbc81508
LorisPercin <loris.percin@outlook.fr>
2024-06-27 09:26:02 +0200

    add basic customer routes

--- paye_ton_kawa/webshop/urls.py
+++ paye_ton_kawa/webshop/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("customer/create", views.index, name="create_customer"),
+    path("customer/find", views.index, name="find_customer"),
+    path("customer/find-by", views.index, name="find_customer"),
+    path("customer/find-all", views.index, name="find_all_customers"),
+    path("customer/update", views.index, name="update_customer"),
+    path("customer/delete", views.index, name="delete_customer"),
 ]
--- paye_ton_kawa/webshop/views.py
+++ paye_ton_kawa/webshop/views.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
 
 from webshop.EntityManager import EntityManager
 
@@ -16,3 +17,32 @@ def index(request: Any) -> HttpResponse:
         "webshop/index.html",
         {"customers": customers, "products": products, "orders": orders},
     )
+
+
+@require_http_methods(['POST'])
+def create_customer(request: Any, attr: dict[str, Any]) -> HttpResponse:
+    return HttpResponse({"success": f"CLient {attr['name']} créé"})
+
+
+@require_http_methods(['POST'])
+def find_customer(request: Any, customer_id: int) -> HttpResponse:
+    return HttpResponse({"success": f"CLient {customer_id} trouvé"})
+
+
+@require_http_methods(['POST'])
+def find_customer_by(request: Any, attr: dict[str, Any]) -> HttpResponse:
+    return HttpResponse({"success": f"CLient {attr['name']} trouvé"})
+
+
+def find_all_customers(request: Any) -> HttpResponse:
+    return HttpResponse({"success": "CLients trouvés"})
+
+
+@require_http_methods(['POST'])
+def update_customer(request: Any, customer_id: int) -> HttpResponse:
+    return HttpResponse({"success": f"CLient {customer_id} mis à jour"})
+
+
+@require_http_methods(['POST'])
+def delete_customer(request: Any, customer_id: int) -> HttpResponse:
+    return HttpResponse({"success": f"CLient {customer_id} supprimé"})
